fbc412980d2c93f7275f862961e377c18e16e1c6
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2025-05-17 23:19:15 +0600

    add env constant

---
+++ src/configs/env.constant.ts
@@ -0,0 +1,6 @@
+export const JWT_SECRET =
+  '910b0922f5e198cdfe22a0caee313d14750044e456dede4f56b031cab674571a073eac2feee82d3c515bc8cb566c394dcc4e4d1c4b46a09bd26d452b7d2287fd';
+export const HEADER_STRING = 'Authorization';
+export const TOKEN_PREFIX = 'Bearer';
+export const EXPIRATION_TIME = 24 * 60 * 60 * 1000;
+export const REFRESH_TOKEN_EXPIRATION_TIME = 24 * 60 * 60 * 1000;
