7f0c864eed19aa23e467f70ff5980bacc1a2600e
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2025-05-17 23:19:34 +0600

    add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './configs/database.config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { STATIC } from './constants/global.constant';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(databaseConfig),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', `${STATIC}`),
+      serveRoot: `/${STATIC}/`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RequestMiddleware)
+      .forRoutes('*')
+      .apply(LoggerMiddleware) // Apply LoggerMiddleware globally
+      .forRoutes('*')
+      .apply(JwtMiddleware) // Apply JwtMiddleware to routes containing "secured"
+      .forRoutes('*secured*')
+      .apply(StaticFileCheckMiddleware)
+      .forRoutes('/static/*'); // Apply to all static file routes;
+  }
+}
